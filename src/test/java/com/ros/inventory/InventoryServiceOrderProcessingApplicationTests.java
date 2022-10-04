6da8a13ba0958a99666506657d55d48e49c272c2
Khizar2234 <114140303+Khizar2234@users.noreply.github.com>
2022-10-04 11:39:21 +0530

    Update InventoryServiceOrderProcessingApplicationTests.java

--- src/test/java/com/ros/inventory/InventoryServiceOrderProcessingApplicationTests.java
+++ src/test/java/com/ros/inventory/InventoryServiceOrderProcessingApplicationTests.java
@@ -1,14 +1,1 @@
-package com.ros.inventory;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-// @SpringBootTest
-// class InventoryServiceOrderProcessingApplicationTests {
-
-// 	@Test
-// 	void contextLoads() {
-// 	}
-
-// }
 
